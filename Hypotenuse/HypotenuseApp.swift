870652a5aa1e718600a1b3b4c4770e0ea870d71f
William Dupont <williamd.contact@gmail.com>
2022-10-08 18:01:43 +0100

    Initial Commit

---
+++ Hypotenuse/HypotenuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypotenuseApp.swift
+//  Hypotenuse
+//
+//  Created by William Dupont on 08/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct HypotenuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
